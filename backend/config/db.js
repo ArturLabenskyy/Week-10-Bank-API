c715145ca5f6e85187b3325d8421c75018f5ae42
ArturLabenskyy <dodik.dod@hotmail.com>
2023-03-24 16:53:16 +0300

    backend fix made

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,15 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.DB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.log(`${error}`);
-    }
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log(`Mongo Connected: ${conn.connection.host}`.cyan.underline.bold);
+  } catch (error) {
+    console.log(`${error}`.red.bold);
+  }
 };
 
 export default connectDB;
